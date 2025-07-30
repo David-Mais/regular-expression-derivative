b7369c6a7770b7d6b32e8eb71d7cbea7e7b37bf0
david-mais <david.maisuradze0920@gmail.com>
2025-07-30 15:32:04 +0400

    add negation in regx type definitiona and methods

--- coq/src/der_proof.v
+++ coq/src/der_proof.v
@@ -12,7 +12,8 @@ Inductive regex : Type :=
 | Alt : regex -> regex -> regex
 | And : regex -> regex -> regex
 | Seq : regex -> regex -> regex
-| Star : regex -> regex.
+| Star : regex -> regex
+| Neg : regex -> regex.
 
 Definition alt (r1 r2 : regex) : regex :=
   match r1, r2 with
@@ -40,6 +41,14 @@ Definition star (r1 : regex) : regex :=
   | _ => Star r1
   end.
 
+Definition neg(r1 : regex) : regex :=
+  match r1 with
+  | Neg r => r
+  | Alt r r' => and_ (Neg r) (Neg r')
+  | And r r' => alt (Neg r) (r')
+  | _ => Neg r1
+  end.
+
 Fixpoint nullable (r : regex) : bool :=
   match r with
   | Empty => false
@@ -49,55 +58,5 @@ Fixpoint nullable (r : regex) : bool :=
   | And r1 r2 => nullable r1 && nullable r2
   | Seq r1 r2 => nullable r1 && nullable r2
   | Star _ => true
+  | Neg r1 => negb (nullable r1)
   end.
-
-Inductive matches : regex -> string -> Prop :=
-| M_Epsilon :
-    matches Epsilon []
-| M_Char : forall c,
-    matches (Char c) [c]
-| M_AltL : forall r1 r2 s,
-    matches r1 s ->
-    matches (Alt r1 r2) s
-| M_AltR : forall r1 r2 s,
-    matches r2 s ->
-    matches (Alt r1 r2) s
-| M_And : forall r1 r2 s,
-    matches r1 s ->
-    matches r2 s ->
-    matches (And r1 r2) s
-| M_Seq : forall r1 r2 s1 s2,
-    matches r1 s1 ->
-    matches r2 s2 ->
-    matches (Seq r1 r2) (s1 ++ s2)
-| M_Star0 : forall r,
-    matches (Star r) []
-| M_Star1 : forall r s1 s2,
-    matches r s1 ->
-    matches (Star r) s2 ->
-    matches (Star r) (s1 ++ s2).
-
-(* Forward direction proof *)
-Theorem nullable_correct_forward : forall r,
-  nullable r = true -> matches r [].
-Proof.
-  intros r H.
-  induction r; simpl in H.
-  - discriminate H.
-  - constructor.
-  - discriminate.
-  - apply orb_true_iff in H as [H1 | H2].
-    + apply M_AltL.
-      apply IHr1. exact H1.
-    + apply M_AltR.
-      apply IHr2. exact H2.
-  - apply andb_true_iff in H as [H1 H2].
-    + apply M_And.
-      apply IHr1. exact H1.
-      apply IHr2. exact H2.
-  - apply andb_true_iff in H as [H1 H2].
-    apply M_Seq with (s1 := []) (s2 := []).
-    + apply IHr1. exact H1.
-    + apply IHr2. exact H2.
-  - constructor.
-Qed.
