a604417b5b1e7aa30c0ca0ab93a48e463f600740
Mariam Katamashvili <katamashvili.mariami@gmail.com>
2025-08-28 14:15:56 +0400

    theorem 4.1 proved

---
+++ coq/src/example_ocaml.ml
@@ -0,0 +1,77 @@
+
+type bool =
+| True
+| False
+
+type nat =
+| O
+| S of nat
+
+type 'a list =
+| Nil
+| Cons of 'a * 'a list
+
+(** val add : nat -> nat -> nat **)
+
+let rec add n m =
+  match n with
+  | O -> m
+  | S p -> S (add p m)
+
+module Nat =
+ struct
+  (** val leb : nat -> nat -> bool **)
+
+  let rec leb n m =
+    match n with
+    | O -> True
+    | S n' -> (match m with
+               | O -> False
+               | S m' -> leb n' m')
+ end
+
+module Example =
+ struct
+  (** val add1 : nat -> nat **)
+
+  let add1 n =
+    S n
+
+  (** val sum : nat list -> nat **)
+
+  let rec sum = function
+  | Nil -> O
+  | Cons (x, xs') -> add x (sum xs')
+
+  (** val map : ('a1 -> 'a2) -> 'a1 list -> 'a2 list **)
+
+  let rec map f = function
+  | Nil -> Nil
+  | Cons (x, xs') -> Cons ((f x), (map f xs'))
+
+  (** val inc_all : nat list -> nat list **)
+
+  let inc_all xs =
+    map add1 xs
+
+  (** val evenb : nat -> bool **)
+
+  let rec evenb = function
+  | O -> True
+  | S n0 -> (match n0 with
+             | O -> False
+             | S k -> evenb k)
+
+  type person = { pid : nat; age : nat }
+
+  (** val age : person -> nat **)
+
+  let age p =
+    p.age
+
+  (** val can_vote : person -> bool **)
+
+  let can_vote p =
+    Nat.leb (S (S (S (S (S (S (S (S (S (S (S (S (S (S (S (S (S (S
+      O)))))))))))))))))) p.age
+ end
---
+++ coq/src/example_ocaml.mli
@@ -0,0 +1,38 @@
+
+type bool =
+| True
+| False
+
+type nat =
+| O
+| S of nat
+
+type 'a list =
+| Nil
+| Cons of 'a * 'a list
+
+val add : nat -> nat -> nat
+
+module Nat :
+ sig
+  val leb : nat -> nat -> bool
+ end
+
+module Example :
+ sig
+  val add1 : nat -> nat
+
+  val sum : nat list -> nat
+
+  val map : ('a1 -> 'a2) -> 'a1 list -> 'a2 list
+
+  val inc_all : nat list -> nat list
+
+  val evenb : nat -> bool
+
+  type person = { pid : nat; age : nat }
+
+  val age : person -> nat
+
+  val can_vote : person -> bool
+ end
